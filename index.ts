b1921f18a18151a593d9278b45b1b3deff77643e
dakodaw <dakodaw@yahoo.com>
2021-01-15 15:11:04 -0700

    Answer last question. Still need to come up with a good example

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { of } from "rxjs";
-import { pairwise } from "rxjs/operators";
+import { pairwise, startWith } from "rxjs/operators";
 
 /**
  * Choose one or more operators from the RxJS Api: https://rxjs-dev.firebaseapp.com/Api
@@ -29,9 +29,12 @@ const operatorName = "pairwise";
  * What does the operator do?
  */
 const blurb =
-  "Pairwise groups pairs of consecutive emissions together and emits them as an array of two values. The observable only emits after the base observable emits twice or more. You can use startWith in conjunction with pairwise to have a default beginning value so the first emission of the base observable makes it through";
+  "Pairwise groups pairs of consecutive emissions together and emits them as an array of two values. The observable only emits after the base observable emits twice or more. If you are unsure if your base observable will emit more than once, you can use 'startWith' in conjunction with 'pairwise' to have a default beginning value so the first emission of the base observable makes it through";
 
-const source$ = of(1, 2, 3).pipe(pairwise());
+const source$ = of(1).pipe(
+  startWith(0),
+  pairwise()
+);
 
 console.log("OPERATOR:", operatorName);
 console.log("Description:", blurb);
@@ -39,13 +42,13 @@ console.log(`1. What effect does multiple subscribers have on 'pairwise'? \n\n
   Answer: Multiple subscribers doesn't have any effect on this operator. It will emit values the same regardless of the number of subscribers`);
 
 console.log(`2. What effect does the subscription completing have on 'pairwise'? \n\n
-  Answer: The subscription completing is actually caused by pairwise, so it doesn't have any effect on it. If you unsubscribe any other way from the source observable, then pairwise won't do anything else or care.`);
+  Answer: The subscription completing is actually caused by 'pairwise', so it doesn't have any effect on it. If you unsubscribe any other way from the source observable, then 'pairwise' won't do anything else or care.`);
 
 console.log(`3. Is the 'pairwise' operator affected differently by hot/cold observables? \n\n
   Answer: It isn't affected differently. When 'pairwise' is in the pipeline, and data runs through it, it will act`);
 
-console.log(`4. In a chain of operators, how does placement of this operator matter? \n\n
-  Answer: `);
+console.log(`4. In a chain of operators, how does placement of 'pairwise' matter? \n\n
+  Answer: It should be placed after you operate on the current value of the base emission because it changes the shape of the values moving forward into an array. If you are wanting to operate on both the current and previous values, those operations should take place after 'pairwise'`);
 
 console.log("OUTPUT:");
 source$.subscribe(x => console.log(x));
